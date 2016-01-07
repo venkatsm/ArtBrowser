c9d2490f00945279eec89492d85e5f32c2425eea
venkatsm <sm.venkat@yahoo.com>
2016-01-08 02:05:17 +0530

    added help module

---
+++ Dev/Src/ArtGallery/ArtGallery/Controllers/HelpController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtGallery.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TermsAndConditions()
+        {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+        public ActionResult GettingStarted()
+        {
+            return View();
+        }
+    }
+}
